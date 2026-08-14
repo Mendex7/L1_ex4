149a0f56d9c539117a4a0c1b019470b265f74945
Paulo Cesar Mendes dos Santos <Paulo Cesar Mendes dos Santos>
2026-08-14 11:15:25 -0300

    modifiquei os erros que estavam errados

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
  
  
 router.get("/", (req, res)=>{
-      res.render("pages/index",{"resultado":null,"valores":{"resul":"digite qual é o resultado"}});  
+      res.render("pages/index",{"resultado":null,"valores":{"resul":""}});  
 });
  
  
